60270de261271b3a8750f593acb9ce7adfc04c57
Nikolas Borrel-Jensen <nikolas.borrel@simulation.science>
2026-05-12 20:32:08 +0200

    use uv in docker

--- CHORAS_BUILD.sh
+++ CHORAS_BUILD.sh
@@ -11,6 +11,9 @@ echo "Docker image exported to dg_image.tar"
 docker save -o backend/app/services/executors/de_image.tar de_image:latest
 echo "Docker image exported to de_image.tar"
 
+docker save -o backend/app/services/executors/deeponet_image.tar deeponet_image:latest
+echo "Docker image exported to deeponet_image.tar"
+
 # docker save -o backend/app/services/executors/pa_image.tar pa_image:latest
 # echo "Docker image exported to pa_image.tar"
 
