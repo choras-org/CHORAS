f8aeb8be5c7a0e030bc92421c04e2ce371318f57
Matteo-Scerbo <matteo.scerbo@hotmail.it>
2026-05-12 15:09:14 +0200

    Docker setup

--- CHORAS_BUILD.sh
+++ CHORAS_BUILD.sh
@@ -11,6 +11,9 @@ echo "Docker image exported to dg_image.tar"
 docker save -o backend/app/services/executors/de_image.tar de_image:latest
 echo "Docker image exported to de_image.tar"
 
+docker save -o backend/app/services/executors/modart_image.tar modart_image:latest
+echo "Docker image exported to modart_image.tar"
+
 # docker save -o backend/app/services/executors/pa_image.tar pa_image:latest
 # echo "Docker image exported to pa_image.tar"
 
