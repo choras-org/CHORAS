3839b8c9449c9b6adf248f555ad809b2c60fa463
Anne Heimes <ahe@akustik.rwth-aachen.de>
2026-05-12 22:28:37 +0200

    add config for sparrowpy integration

--- CHORAS_BUILD.sh
+++ CHORAS_BUILD.sh
@@ -14,4 +14,10 @@ echo "Docker image exported to de_image.tar"
 docker save -o backend/app/services/executors/pyroomacoustics_image.tar pyroomacoustics_image:latest
 echo "Docker image exported to pyroomacoustics_image.tar"
 
+docker save -o backend/app/services/executors/sparrowpy_image.tar sparrowpy_image:latest
+echo "Docker image exported to sparrowpy_image.tar"
+
+# docker save -o backend/app/services/executors/pa_image.tar pa_image:latest
+# echo "Docker image exported to pa_image.tar"
+
 docker compose up
